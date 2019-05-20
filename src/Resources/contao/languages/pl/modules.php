98d45c0a2925a233635cffd7492e3ecd93ed0424
Kuba <kuba@hotaru.pl>
2019-05-20 15:29:36 +0200

    Add polish language support

---
+++ src/Resources/contao/languages/pl/modules.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * @copyright  trilobit GmbH
+ * @author     trilobit GmbH <https://github.com/trilobit-gmbh>
+ * @license    LGPL-3.0-or-later
+ * @link       http://github.com/trilobit-gmbh/contao-constants-bundle
+ */
+
+// MOD
+$GLOBALS['TL_LANG']['MOD']['tl_constants'][0] = 'Stałe wartości';
+$GLOBALS['TL_LANG']['MOD']['tl_constants'][1] = 'Załączaj stałe wartości';
