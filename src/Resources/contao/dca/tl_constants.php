dd733891dbd35043759957d34d172f2508b86f31
trilobit-gmbh <trilobit-gmbh@users.noreply.github.com>
2022-03-10 18:43:08 +0100

    Php8 contao413 (#14)
    * fixed missing array index check
    
    * update readme and composer.json
    
    Co-authored-by: Dennis Patzer <d.patzer@heimrich-hannot.de>

--- src/Resources/contao/dca/tl_constants.php
+++ src/Resources/contao/dca/tl_constants.php
@@ -71,7 +71,7 @@ $GLOBALS['TL_DCA']['tl_constants'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_constants']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if (!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\')) return false; Backend.getScrollOffset();"',
+                'attributes' => 'onclick="if (!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\')) return false; Backend.getScrollOffset();"',
             ],
             'toggle' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_constants']['toggle'],
